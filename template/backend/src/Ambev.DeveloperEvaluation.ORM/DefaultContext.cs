eaee532df3e4e7813b636c1409435b2588a52b35
Enio Marley <enio.marley@hotmail.com>
2025-04-12 08:59:02 -0400

    chore: Adicionado pacote do Tools e ajuste na migrationAssembly

--- template/backend/src/Ambev.DeveloperEvaluation.ORM/DefaultContext.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/DefaultContext.cs
@@ -33,8 +33,8 @@ public class YourDbContextFactory : IDesignTimeDbContextFactory<DefaultContext>
         var connectionString = configuration.GetConnectionString("DefaultConnection");
 
         builder.UseNpgsql(
-               connectionString,
-               b => b.MigrationsAssembly("Ambev.DeveloperEvaluation.WebApi")
+            connectionString,
+            b => b.MigrationsAssembly("Ambev.DeveloperEvaluation.ORM")
         );
 
         return new DefaultContext(builder.Options);
